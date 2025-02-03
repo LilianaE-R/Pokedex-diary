bf34d1e70f041e296ea0790fd1b69944af106713
Arokh-web <michaelabudich@gmail.com>
2025-02-03 18:49:09 +0100

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 // Importing all necessary functions:
 
-import { searchBar, fetchDataComplete } from "./modules/network.mjs";
+import { search, fetchData } from "./modules/network.mjs";
 
 // Executing functions:
 
-const search = document.getElementById("searchbar");
-const listContainer = document.getElementById("content-container");
-websiteStart();
-
-function websiteStart() {
-  searchBar();
-  if (search.value) {
-    listContainer.innerHTML = "";
-  } else {
-    fetchDataComplete();
-  }
-}
+fetchData();
--- src/modules/network.mjs
+++ src/modules/network.mjs
@@ -4,91 +4,84 @@ import { createCard } from "./ui.mjs";
 
 export const mainSRC = "https://pokeapi.co/api/v2/pokemon";
 const error = document.getElementById("errorbar");
+const searchBar = document.getElementById("searchbar");
+const listContainer = document.getElementById("content-container");
 
-export function searchBar() {
-  const searchBar = document.getElementById("searchbar");
-  const uniqueNum = new Set();
-  const listContainer = document.getElementById("content-container");
+export const search = function search(e) {
+  let search = searchBar.value;
 
-  searchBar.addEventListener("input", () => {
-    listContainer.innerHTML = "";
-    const userSearch = searchBar.value.trim();
-    // Single No Input
-    if (!isNaN(userSearch)) {
-      const poke_no = parseInt(userSearch, 10);
-      if (!uniqueNum.has(poke_no)) {
-        uniqueNum.add(poke_no);
-        // Fetch-Single-Start
-        fetchDataFromSearchID(poke_no);
-      }
-      // Multi No Input
-    } else if (userSearch.split(",").every((i) => !isNaN(Number(i.trim())))) {
-      const poke_no = userSearch
-        .split(",")
-        .map((i) => Number(i.trim()))
-        .filter((num) => !isNaN(num));
-      // Fetch-Multi-Start
-      poke_no.forEach((i) => {
-        if (!uniqueNum.has(i)) {
-          uniqueNum.add(i);
-          fetchDataFromSearchID(i);
-        }
-      });
-    } else {
-      const poke_name = userSearch;
-      fetchDataFromSearchName(poke_name);
-    }
-  });
-}
-
-// fetch-cycle for search by ID
-export async function fetchDataFromSearchID(poke_no) {
-  try {
-    const res = await fetch(`${mainSRC}/${poke_no}`);
-    if (!res.ok) throw new Error("Something went wrong");
-    const data = await res.json();
-    console.log(`That worked: Pokemon Number ${poke_no}`, data);
-    errorbar.innerHTML = "";
-    createCard(data);
-  } catch (e) {
-    console.error(e);
-    console.error(`Error at index ${poke_no}: `, e);
-    errorbar.innerHTML = "Pokemon does not exist!";
+  if (!search) {
+    searchBar.placeholder = "Please insert a Poke-ID or Poke-Name.";
+    fetchData.complete();
+  } else if (!isNaN(search)) {
+    fetchData.searchById(search);
+  } else if (search.includes(",")) {
+    let ids = search
+      .split(",")
+      .map((no) => no.trim())
+      .filter((no) => !isNaN(no));
+    ids.forEach((id) => fetchData.searchById(id));
+  } else {
+    console.log("Insert a value");
   }
-}
+};
 
-// fetch-cycle for search by ID
-export async function fetchDataFromSearchName(poke_name) {
-  try {
-    const res = await fetch(`${mainSRC}/${poke_name}`);
-    if (!res.ok) throw new Error("Something went wrong");
-    const data = await res.json();
-    console.log(
-      `That worked: It is Pokemon No: ${data.id} with the name ${data.name}`
-    );
-    errorbar.innerHTML = "";
-    createCard(data);
-  } catch (e) {
-    console.error(e);
-    console.error(`Error at index ${poke_name}: `, e);
-    errorbar.innerHTML = "Pokemon does not exist!";
-  }
-}
-
-export async function fetchDataComplete() {
-  const length = 1000;
-
-  const res = await Promise.all(
-    Array.from({ length }, (_, i) =>
-      fetch(`${mainSRC}/${i + 1}`)
-        .then((res) => (res.ok ? res.json() : null))
-        .catch(() => null)
-    )
+function searchById(searchID) {
+  dataArray.find((pokemon) =>
+    pokemon.id == searchID ? createCard(id) : (error.textContent = "Insert sth")
   );
 
-  const dataArray = res.filter(Boolean);
-  console.log(dataArray);
-  dataArray.forEach((pokemon) => {
-    createCard(pokemon);
-  });
+  export async function fetchData() {
+    const length = 1000;
+
+    const res = await Promise.all(
+      Array.from({ length }, (_, i) =>
+        fetch(`${mainSRC}/${i + 1}`)
+          .then((res) => (res.ok ? res.json() : null))
+          .catch(() => null)
+      )
+    );
+    const dataArray = res.filter(Boolean);
+    console.log(dataArray);
+    dataArray.forEach((pokemon) => {
+      createCard(pokemon);
+    });
+    return dataArray;
+  }
 }
+
+// export function searchBar() {
+//   const searchBar = document.getElementById("searchbar");
+//   const uniqueNum = new Set();
+//   const listContainer = document.getElementById("content-container");
+
+//   searchBar.addEventListener("input", () => {
+//     listContainer.innerHTML = "";
+//     const userSearch = searchBar.value.trim();
+//     // Single No Input
+//     if (!isNaN(userSearch)) {
+//       const poke_no = parseInt(userSearch, 10);
+//       if (!uniqueNum.has(poke_no)) {
+//         uniqueNum.add(poke_no);
+//         // Fetch-Single-Start
+//         fetchDataComplete.searchById(poke_no);
+//       }
+//       // Multi No Input
+//     } else if (userSearch.split(",").every((i) => !isNaN(Number(i.trim())))) {
+//       const poke_no = userSearch
+//         .split(",")
+//         .map((i) => Number(i.trim()))
+//         .filter((num) => !isNaN(num));
+//       // Fetch-Multi-Start
+//       poke_no.forEach((i) => {
+//         if (!uniqueNum.has(i)) {
+//           uniqueNum.add(i);
+//           fetchDataComplete.searchById(i);
+//         }
+//       });
+//     } else {
+//       const poke_name = userSearch;
+//       fetchDataFromSearchName(poke_name);
+//     }
+//   });
+// }
